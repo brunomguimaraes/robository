6cca7615684280b9cdd8f990f6ab7ef57b2bc86e
Bruno <mguimaraesbruno@gmail.com>
2020-04-04 19:22:31 -0300

    Updates

---
+++ precache-manifest.d1588c10ee8962ea112d2bb2bf3d138a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66dc65f6ab27562bec3d026a447573a9",
+    "url": "/robository/index.html"
+  },
+  {
+    "revision": "2991eae23e5ff61f8b6f",
+    "url": "/robository/static/css/2.ad8fe80d.chunk.css"
+  },
+  {
+    "revision": "919436f4413260294556",
+    "url": "/robository/static/css/main.2f2d26ac.chunk.css"
+  },
+  {
+    "revision": "2991eae23e5ff61f8b6f",
+    "url": "/robository/static/js/2.d625e97b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/robository/static/js/2.d625e97b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "919436f4413260294556",
+    "url": "/robository/static/js/main.ae1f2420.chunk.js"
+  },
+  {
+    "revision": "3349b5609e602613cbda",
+    "url": "/robository/static/js/runtime-main.ec6c0205.js"
+  },
+  {
+    "revision": "cf58e8533f5d3bdff75366d5ad5df046",
+    "url": "/robository/static/media/gatekeeper.cf58e853.gif"
+  },
+  {
+    "revision": "1e40420a18ac9f082d633b144f36b3cf",
+    "url": "/robository/static/media/retro_computer.1e40420a.ttf"
+  }
+]);
