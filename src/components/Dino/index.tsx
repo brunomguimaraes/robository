aa882933ee3865b4b8d2069f8272e63b0fbc5238
Bruno <mguimaraesbruno@gmail.com>
2020-04-04 20:22:11 -0300

    dine speech added

--- src/components/Dino/index.tsx
+++ src/components/Dino/index.tsx
@@ -6,7 +6,11 @@ import './style.css';
 function Dinotron() {
   return (
     <div className="dinotron">
-      <div>I am Dinotron</div>
+      <div className="dinotronHeader">
+        <div className="dinotronSpeech" >I am Dinotron, i love eating barbecue! I've been barbecuing since the post-jurrasic-cyber-gama
+          age. Oh, you don't know what i'm talking about? You are not from the future?!</div>
+        <img src={require('../../assets/robot-gifs/dino.gif')} alt="dino-say-hi" className="dinoGif" />
+      </div>
     </div>
   );
 }
